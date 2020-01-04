559a94042677da34bdb57423b57c9ba488966efa
Chase Dickinson <chase.dickinson@gmail.com>
2020-01-04 10:45:36 -0600

    updating format

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = "= 0.12.18"
 
   backend "remote" {
-    hostname     = "app.terraform.io"
+    hostname = "app.terraform.io"
   }
 }
 
 provider "aws" {
   version = "=2.43"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
