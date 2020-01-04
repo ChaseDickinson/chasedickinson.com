86d388af30d7600849dba471d47d657c1a2b0d0b
Chase Dickinson <chase.dickinson@gmail.com>
2020-01-04 09:37:54 -0600

    latest test

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,12 @@ resource "aws_s3_bucket" "web_origin" {
   bucket = "${data.aws_caller_identity.current.account_id}-web-origin"
   acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.web_origin.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,4 +13,6 @@ terraform {
 
 provider "aws" {
   version = "=2.43"
+
+  region = "us-east-1"
 }
