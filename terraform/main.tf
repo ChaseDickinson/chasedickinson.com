d9d00229af4045d00bf3885b69b20898013470a9
Chase Dickinson <chase.dickinson@gmail.com>
2020-01-04 14:31:34 -0600

    added certificate

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,21 @@ resource "aws_acm_certificate" "this" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.this.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.this.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.registered.id
+  records = [aws_acm_certificate.this.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+
+//TODO
+//  - CloudFront distribution
+//  - Web content
+//  - GitHub Actions
