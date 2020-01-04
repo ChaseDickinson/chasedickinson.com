c3d6d8f1032b70e1cacee85e785212b1403e1d4d
Chase Dickinson <chase.dickinson@gmail.com>
2020-01-04 14:01:27 -0600

    checking in latest

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
 data "aws_caller_identity" "current" {}
 
+data "aws_route53_zone" "registered" {
+  name = "chasedickinson.com."
+}
+
 resource "aws_s3_bucket" "web_origin" {
   bucket = "${data.aws_caller_identity.current.account_id}-web-origin"
   acl    = "private"
@@ -12,4 +16,13 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_acm_certificate" "this" {
+  domain_name       = substr(data.aws_route53_zone.registered.name, 0, length(data.aws_route53_zone.registered.name)-1)
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
